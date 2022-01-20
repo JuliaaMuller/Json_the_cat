7b3432d0017418e5fd23e8ba4ecb734119a00fac
JuliaaMuller <julialm.muller@gmail.com>
2022-01-20 14:22:03 -0500

    refactored and test file

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,12 +1,14 @@
 const request = require('request');
-const args = process.argv.slice(2);
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${args[0]}`, (error, response, body) => {
-  const data = JSON.parse(body);
-  if (data.length === 0) {
-    console.log(`${args[0]} is not found.`);
-  } else if (error !== null) {
-    console.log(error);
-  }
-  console.log(data[0]['description']);
-});
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      callback('Breed not found', null);
+    } else {
+      callback(null, data[0]['description']);
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
